5adc7a8265bdaba524f1dfcd90c3b0199d257993
Sourabhsingh345 <sulabhsingh541@gmail.com>
2026-07-30 14:24:37 +0530

    MongoDB Atlas and Render

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/images", express.static(path.resolve(__dirname, "../frontend/images")));
 
-mongoose.connect("mongodb://127.0.0.1:27017/fooddelivery")
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("✅ MongoDB Connected");
 })
@@ -292,6 +292,8 @@ app.delete("/orders", async (req, res) => {
 
 });
 
-app.listen(5000, () => {
-    console.log("🚀 Server Running On Port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server Running On Port ${PORT}`);
 });
